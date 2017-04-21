7a5142fe0c06947ce56795ea4916a6e8e49322a1
Michael O'Keefe <mokeefe@mitre.org>
2017-04-21 14:03:11 -0400

    start of clinical randomizer

---
+++ lib/cypress/clinical_randomizer.rb
@@ -0,0 +1,14 @@
+module Cypress
+  class ClinicalRandomizer
+    def self.randomize(record, prng = Random.new)
+
+    end
+
+    private
+
+    def self.split_by_date(record, prng)
+      new_record = record.clone
+      
+    end
+  end
+end
